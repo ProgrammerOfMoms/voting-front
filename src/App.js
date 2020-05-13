dd73a6e30d4d4efa321e632270c061b0915dd40b
admin <admin@LAPTOP-98V9G3FJ>
2020-05-13 22:29:25 +0900

    1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/start">
+        <Route path="/start:id">
           <MainPage auth={false}/>
         </Route>
         <Route path="/candidates">
